7490e6632b6df01b4781d0a4185e19c18bca2383
ParmarNeha0808 <109799963+ParmarNeha0808@users.noreply.github.com>
2022-08-02 17:25:10 +0530

    Add files via upload

---
+++ Assignment/Module-3.1/year and days.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int days,years,weeks;
+	
+	printf("enter the value:");
+	scanf("%d",&days);
+	
+	years = (days/365);	
+	weeks = (days%365)/7;
+	days = (days%365)%7;
+	
+	
+     printf("years:%d\n",years);
+     printf("weeks:%d\n",weeks);
+     printf("days: %d",days);
+     
+   	return 0;
+   }
