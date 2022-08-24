8093db4399d1f2bf1ee487719eef5bbf30a3a2c6
ParmarNeha0808 <109799963+ParmarNeha0808@users.noreply.github.com>
2022-08-24 17:38:00 +0530

    Add files via upload

---
+++ Task/without without.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void data(void)
+{
+	int id;
+	printf("enter the id:");
+	scanf("%d",&id);
+	printf("\nvalue of id is%d",id);
+}
+int main()
+{
+	int no;
+	printf("Enter the value of no:");
+	scanf("%d",&no);
+	data();
+}
+
