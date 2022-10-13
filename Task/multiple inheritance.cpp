bb06535102ed07db686a3560d95986a565f02c43
ParmarNeha0808 <109799963+ParmarNeha0808@users.noreply.github.com>
2022-10-12 21:43:20 -0700

    Add files via upload

---
+++ Task/multiple inheritance.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class A
+{
+	public:
+		int aid;
+		string anm;
+			void get()
+			{
+			cout<<"Enter the aid:";
+			cin>>aid;
+			cout<<"enter the aname:";
+			cin>>anm;
+		}
+};
+class B
+{
+	public:
+		int bid;
+	    string bnm;
+		
+			void getdata()
+			{
+			cout<<"Enter the bid:";
+			cin>>bid;
+			cout<<"Enter the bname:";
+			cin>>bnm;
+		}
+};
+class tops: public A, public B
+{
+	public:
+	void showdata()
+	{
+	cout<<endl<<"-------------"<<anm<<"---------------";
+	cout<<endl<<"your id is:"<<aid;
+	cout<<endl<<"---------------"<<bid<<"---------------";
+	cout<<endl<<"your id is:"<<bid;
+	}
+};
+int main()
+{
+	tops t1;
+	t1.get();
+	t1.getdata();
+	t1.showdata();
+}
